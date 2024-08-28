2ad540973a1ffa5e20959bc8b405fdf55fb942e7
Kevin Bonilla <kb1909zzz@gmail.com>
2024-08-28 16:25:52 -0600

    build(base project): website

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Button from "./Button.astro";
+import Navbar from "./Navbar.astro";
+import Benefits from "./Benefits.astro";
+import Process from "./Process.astro";
+import Footer from "./Footer.astro";
+import Card from "./Card.astro";
+
+export {
+  Button, Navbar, Benefits, Process, Footer, Card
+}
---
+++ src/data/data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    title: "Creación de Páginas Web",
+    text: "Diseñamos páginas web impactantes y optimizadas para captar la atención de tus clientes desde el primer momento. Nuestras landing pages están orientadas a convertir visitantes en clientes, con diseños modernos y funcionales adaptados a las necesidades de cada negocio.",
+    img: "/src/img/astro.svg"
+  },
+  {
+    title: "Seguridad Digital con Proton",
+    text: "Garantizamos la máxima seguridad digital para tu empresa mediante la integración de soluciones avanzadas de Proton, incluyendo correo electrónico seguro, VPN y almacenamiento encriptado. Protegemos tus datos con tecnologías de cifrado de vanguardia.",
+    img: "/src/img/mail.svg"
+  },
+  {
+    title: "Gestión y Almacenamiento con Nextcloud",
+    text: "Ofrecemos soluciones de colaboración y almacenamiento de archivos utilizando Nextcloud, una plataforma de código abierto que asegura que tus datos estén siempre bajo tu control. Comparte y gestiona documentos de manera segura y eficiente, con total transparencia.",
+    img: "/src/img/nextcloud.svg"
+  }
+]
