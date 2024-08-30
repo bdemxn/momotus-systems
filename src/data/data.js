c844655a9d21cc59b7a2481a7fc926b9e1d62d37
Kevin Bonilla <kb1909zzz@gmail.com>
2024-08-30 17:40:59 -0600

    fix(images and css fix features)!: change css/img

--- src/data/data.js
+++ src/data/data.js
@@ -1,17 +1,21 @@
+import astro from "../img/astro.svg";
+import mail from "../img/mail.svg";
+import nextcloud from "../img/nextcloud.svg";
+
 export const data = [
   {
     title: "Creación de Páginas Web",
     text: "Diseñamos páginas web impactantes y optimizadas para captar la atención de tus clientes desde el primer momento. Nuestras landing pages están orientadas a convertir visitantes en clientes, con diseños modernos y funcionales adaptados a las necesidades de cada negocio.",
-    img: "/src/img/astro.svg"
+    img: astro
   },
   {
     title: "Seguridad Digital con Proton",
     text: "Garantizamos la máxima seguridad digital para tu empresa mediante la integración de soluciones avanzadas de Proton, incluyendo correo electrónico seguro, VPN y almacenamiento encriptado. Protegemos tus datos con tecnologías de cifrado de vanguardia.",
-    img: "/src/img/mail.svg"
+    img: mail
   },
   {
     title: "Gestión y Almacenamiento con Nextcloud",
     text: "Ofrecemos soluciones de colaboración y almacenamiento de archivos utilizando Nextcloud, una plataforma de código abierto que asegura que tus datos estén siempre bajo tu control. Comparte y gestiona documentos de manera segura y eficiente, con total transparencia.",
-    img: "/src/img/nextcloud.svg"
+    img: nextcloud
   }
 ]
---
+++ src/img/index.js
@@ -0,0 +1,18 @@
+import astro from "./astro.svg";
+import check from "./check.svg";
+import downArrow from "./down-arrow.svg";
+import ecommerce from "./ecommerce.png";
+import logo from "./logo.svg";
+import mail from "./mail.svg";
+import nextcloud from "./nextcloud.svg";
+import opensource from "./opensource.svg";
+import programming from "./programming.svg";
+import proton from "./proton.svg";
+import security from "./security.png";
+import server from "./server.png";
+import website from "./website.png";
+import why from "./why.png";
+
+export {
+  astro, check, downArrow, ecommerce, logo, mail, nextcloud, opensource, programming, proton, security, server, website, why
+}
